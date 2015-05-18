48e9dca6ca5c97ef6cd561fd9f04bf89621abc72
Gomhon <tonybabich@gmail.com>
2015-05-18 14:21:04 -0700

    fix spelling error

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  resource :albums
+  resources :albums
   get 'home/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
